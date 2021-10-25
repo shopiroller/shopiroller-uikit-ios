320628cdada7151e642a2b5016fe35710c2e418e
abdllhyalcn <a.yalcn@outlook.com>
2021-10-25 16:50:16 +0300

    Address setup added

--- Shopiroller/Classes/Scenes/Address List/Views/Cells/AddressTableViewCell.swift
+++ Shopiroller/Classes/Scenes/Address List/Views/Cells/AddressTableViewCell.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class AddressTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet private weak var confirmView: UIView!
+    @IBOutlet private weak var informationView: UIView!
+    
+    @IBOutlet private weak var confirmTitle: UILabel!
+    @IBOutlet private weak var confirmCancelButton: UIButton!
+    @IBOutlet private weak var confirmDeleteButton: UIButton!
+    @IBOutlet private weak var informationTitle: UILabel!
+    @IBOutlet private weak var informationName: UILabel!
+    
+    @IBOutlet private weak var informationAddress: UILabel!
+    @IBOutlet private weak var informationTown: UILabel!
+    @IBOutlet private weak var informationPhone: UILabel!
+    @IBOutlet private weak var informationTax: UILabel!
+    @IBOutlet private weak var informationDeleteButton: UIButton!
+    @IBOutlet private weak var informationEditButton: UIButton!
+    
+    
+    
+    
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
