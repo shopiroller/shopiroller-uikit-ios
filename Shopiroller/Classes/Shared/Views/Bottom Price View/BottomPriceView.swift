f6ac5e8da34b8c5bc76934d9364806f0d1494c9f
gorkemgur <gorkem.gur@mobiroller.com>
2021-12-27 17:09:16 +0300

    Refactored Bottom Price View
    Also Added Locazited Price View

--- Shopiroller/Classes/Shared/Views/Bottom Price View/BottomPriceView.swift
+++ Shopiroller/Classes/Shared/Views/Bottom Price View/BottomPriceView.swift
@@ -10,32 +10,32 @@ import UIKit
 
 class BottomPriceView: BaseView {
 
-    @IBOutlet private weak var subTotal: UILabel!
-    @IBOutlet private weak var shipping: UILabel!
-    @IBOutlet private weak var total: UILabel!
-    @IBOutlet private weak var price: UILabel!
+    @IBOutlet private weak var subTotalPriceLabel: UILabel!
+    @IBOutlet private weak var shippingPriceLabel: UILabel!
+    @IBOutlet private weak var totalLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
     
     func setup(model: BottomPriceModel) {
         super.setup()
         
-        subTotal.textColor = .textSecondary
-        subTotal.font = .regular14
-        shipping.textColor = .textSecondary
-        shipping.font = .regular14
-        total.textColor = .textSecondary
-        total.font = .regular12
-        price.textColor = .textPrimary
-        price.font = .bold24
+        subTotalPriceLabel.textColor = .textSecondary
+        subTotalPriceLabel.font = .regular14
+        shippingPriceLabel.textColor = .textSecondary
+        shippingPriceLabel.font = .regular14
+        totalLabel.textColor = .textSecondary
+        totalLabel.font = .regular12
+        priceLabel.textColor = .textPrimary
+        priceLabel.font = .bold24
  
-        subTotal.text = "bottom_price_view_subtotal".localized + ECommerceUtil.getFormattedPrice(price: model.subTotalPrice, currency: model.currency)
+        subTotalPriceLabel.text = "bottom_price_view_subtotal".localized + ECommerceUtil.getFormattedPrice(price: model.subTotalPrice, currency: model.currency)
         
         let shippingPrice = ECommerceUtil.getFormattedPrice(price: model.shippingPrice, currency: model.currency)
         
-        shipping.text = "bottom_price_view_shipping".localized + shippingPrice
+        shippingPriceLabel.text = "bottom_price_view_shipping".localized + shippingPrice
         
-        total.text = "bottom_price_view_total".localized
+        totalLabel.text = "bottom_price_view_total".localized
 
-        price.text = ECommerceUtil.getFormattedPrice(price: model.totalPrice, currency: model.currency)
+        priceLabel.text = ECommerceUtil.getFormattedPrice(price: model.totalPrice, currency: model.currency)
     }
 
 }
