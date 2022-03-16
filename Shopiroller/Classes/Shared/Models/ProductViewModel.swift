ac30d0228163b07929646f5733e068d9bfe37b10
gorkemgur <86607113+gorkemgur@users.noreply.github.com>
2022-03-16 18:21:53 +0300

    Fixed isUseFixPrice Bug

--- Shopiroller/Classes/Shared/Models/ProductViewModel.swift
+++ Shopiroller/Classes/Shared/Models/ProductViewModel.swift
@@ -51,11 +51,15 @@ class ProductViewModel {
     
     func isUseFixPrice() -> Bool {
         if let productDetailModel = productDetailModel {
-            return productDetailModel.useFixPrice == true
+            return (productDetailModel.useFixPrice == true && productDetailModel.shippingPrice == 0.0)
         } else if let productListModel = productListModel {
             return productListModel.shippingPrice != 0
         }
         return false
+    }
+    
+    func hasSituation() -> Bool {
+        return (isShippingFree() || isOutofStock()) && !isUseFixPrice()
     }
     
     func isOutofStock() -> Bool {
