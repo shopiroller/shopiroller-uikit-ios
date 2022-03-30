482e299b3dfb737a13fcc49c82a123e4c008fd9c
gorkemgur <86607113+gorkemgur@users.noreply.github.com>
2022-03-30 14:56:07 +0300

    Fixed Usage Of Setup.sh

--- Shopiroller/setup-git-hook.sh
+++ Shopiroller/setup-git-hook.sh
@@ -1,2 +1,4 @@
-ln -s .hooks/pre-commit ../.git/hooks/pre-commit
-sudo chmod +x .hooks/*
+cd ~/Documents/Github/shopiroller_ios/Shopiroller
+ln -s -f .hooks/pre-commit ../.git/hooks/pre-commit
+git config core.hooksPath ~/Documents/GitHub/shopiroller_ios/Shopiroller/.hooks
+sudo chmod +x ~/Documents/GitHub/shopiroller_ios/Shopiroller/.hooks
