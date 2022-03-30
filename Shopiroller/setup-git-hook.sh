e2bd6ad4fd5f3fa9fcd7be411c3d1fc98144c25a
gorkemgur <86607113+gorkemgur@users.noreply.github.com>
2022-03-30 15:47:06 +0300

    Integration completed about the find path

--- Shopiroller/setup-git-hook.sh
+++ Shopiroller/setup-git-hook.sh
@@ -1,4 +1,3 @@
-cd ~/Documents/Github/shopiroller_ios/Shopiroller
 ln -s -f .hooks/pre-commit ../.git/hooks/pre-commit
-git config core.hooksPath ~/Documents/GitHub/shopiroller_ios/Shopiroller/.hooks
-sudo chmod +x ~/Documents/GitHub/shopiroller_ios/Shopiroller/.hooks
+git config core.hooksPath $(pwd)/.hooks
+sudo chmod +x $(pwd)/.hooks
