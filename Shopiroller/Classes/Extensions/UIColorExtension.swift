1398d90630f008317349848d3f23cf233fea4a74
gorkemgur <86607113+gorkemgur@users.noreply.github.com>
2022-10-14 16:03:11 +0300

    Removed UnUsed func

--- Shopiroller/Classes/Extensions/UIColorExtension.swift
+++ Shopiroller/Classes/Extensions/UIColorExtension.swift
@@ -69,37 +69,6 @@ extension UIColor {
     
     static let lightGray = UIColor(named: "lightGray")!
     
-    public static func ajk(withFilenameString filenameString: String, bundle: Bundle) {
-        
-        print(filenameString)
-        
-        guard let pathForResourceString = bundle.path(forResource: filenameString, ofType: nil) else {
-            print("UIFont+:  Failed to register font - path for resource not found.")
-            return
-        }
-        
-        guard let fontData = NSData(contentsOfFile: pathForResourceString) else {
-            print("UIFont+:  Failed to register font - font data could not be loaded.")
-            return
-        }
-        
-        guard let dataProvider = CGDataProvider(data: fontData) else {
-            print("UIFont+:  Failed to register font - data provider could not be loaded.")
-            return
-        }
-        
-        guard let font = CGFont(dataProvider) else {
-            print("UIFont+:  Failed to register font - font could not be loaded.")
-            return
-        }
-        
-        var errorRef: Unmanaged<CFError>?
-        if (CTFontManagerRegisterGraphicsFont(font, &errorRef) == false) {
-            print("UIFont+:  Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
-        }
-    }
-    
-    
 }
 
 
